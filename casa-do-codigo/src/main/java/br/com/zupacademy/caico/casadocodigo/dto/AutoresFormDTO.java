9380d61bb7c6cf4f9ad3c6d1b639134caaad913d
Caico Costa <sales.c.costa@gmail.com>
2021-06-25 12:17:09 -0300

    Remoção dos sets da classe DTO.

--- casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/dto/AutoresFormDTO.java
+++ casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/dto/AutoresFormDTO.java
@@ -31,25 +31,16 @@ public class AutoresFormDTO {
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public String getDescricao() {
 		return descricao;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}	
 	
 	/**
 	 * Converte os dados para a classe Autores, e realiza a inserção no banco de dados.
