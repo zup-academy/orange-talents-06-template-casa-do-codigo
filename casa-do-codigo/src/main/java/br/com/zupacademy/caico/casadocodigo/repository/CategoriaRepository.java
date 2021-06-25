8dc9dbfe4807124127d642f419c3dcfb0f20f440
Caico Costa <sales.c.costa@gmail.com>
2021-06-25 16:22:09 -0300

    Criação de validação para nomes duplicados na categoria.

--- casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/repository/CategoriaRepository.java
+++ casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/repository/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package br.com.zupacademy.caico.casadocodigo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.zupacademy.caico.casadocodigo.model.Categorias;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categorias, Long> {
+
+	Optional<Categorias> findByNome(String nome);
 
 }
