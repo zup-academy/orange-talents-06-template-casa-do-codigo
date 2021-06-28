e24732123928167c0ef24f00731231e76e341e7e
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-06-28 13:58:00 -0300

    comite inicial

---
+++ casadocodigo/casadocodigo/src/main/java/com/br/zupedu/casadocodigo/CasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package com.br.zupedu.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApplication.class, args);
+	}
+
+}
---
+++ casadocodigo/casadocodigo/src/test/java/com/br/zupedu/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zupedu.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
