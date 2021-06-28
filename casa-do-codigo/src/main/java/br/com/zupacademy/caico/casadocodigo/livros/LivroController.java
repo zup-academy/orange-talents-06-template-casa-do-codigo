4892b7ac1cf4771bbc733c54b8dfd11a4b9355f3
Caico Costa <sales.c.costa@gmail.com>
2021-06-28 19:25:57 -0300

    Adicionado endpoint para listar livros cadastrados.

--- casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/livros/LivroController.java
+++ casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/livros/LivroController.java
@@ -1,10 +1,18 @@
 package br.com.zupacademy.caico.casadocodigo.livros;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +24,30 @@ public class LivroController {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	@PostMapping
-	@Transactional
 	public LivroRespDTO cadastrar(@RequestBody @Valid LivroFormDTO livroFormDTO) {		
-		LivroRespDTO livro = livroFormDTO.cadastrar(livroFormDTO,entityManager);
+		LivroRespDTO livro = livroFormDTO.cadastrar(
+				livroFormDTO,
+				entityManager);
 		return livro;		
+	}
+	
+	@GetMapping 
+	public Page<ListaLivros> listarLivros(@PageableDefault(size = 3) Pageable paginacao) {
+		Page<Livros> livros = livroRepository.findAll(paginacao);
+		List<ListaLivros> listaLivros = new ArrayList<>();
+		
+		for (Livros lista : livros) {
+			ListaLivros itemLivro = new ListaLivros();
+			itemLivro.setId(lista.getId());
+			itemLivro.setNome(lista.getTitulo());
+			listaLivros.add(itemLivro);
+		}
+		
+		Page<ListaLivros> livrosCadastrados = new PageImpl<>(listaLivros);
+		return livrosCadastrados;
 	}
 }
