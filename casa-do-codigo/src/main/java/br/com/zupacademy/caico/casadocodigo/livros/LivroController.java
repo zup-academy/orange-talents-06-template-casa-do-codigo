3fa9a9f2d45531b106b158b6664059d0b91e2af8
Caico Costa <sales.c.costa@gmail.com>
2021-06-29 09:57:45 -0300

    EndPoint detalhamento dos livros
    Criação de endpoint para detalhamento dos livros.

--- casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/livros/LivroController.java
+++ casa-do-codigo/src/main/java/br/com/zupacademy/caico/casadocodigo/livros/LivroController.java
@@ -12,7 +12,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +51,20 @@ public class LivroController {
 		
 		Page<ListaLivros> livrosCadastrados = new PageImpl<>(listaLivros);
 		return livrosCadastrados;
+	}
+	
+	@GetMapping("/{id}")
+	private ResponseEntity<LivroDetalhe> detalhamentoLivro(@PathVariable Long id){
+		
+		Livros livro = entityManager.find(Livros.class, id);
+
+		if (livro == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		LivroDetalhe livroDetalhe = new LivroDetalhe(livro);
+		
+		return ResponseEntity.ok().body(livroDetalhe);
+		
 	}
 }
