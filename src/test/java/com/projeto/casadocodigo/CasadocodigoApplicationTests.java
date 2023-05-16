a0d8c922b522bc53cbda4b274b6fd81c9d659f9f
viniciuspch <vinicius.geral2018@gmail.com>
2023-05-16 12:16:22 -0300

    First commit

---
+++ src/test/java/com/projeto/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
