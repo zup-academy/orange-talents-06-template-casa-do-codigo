af8087e100cc747cb4934c3a5c16cadb85533ae6
viniciuspch <vinicius.geral2018@gmail.com>
2023-05-22 15:02:23 -0300

    Criar interface AutorRepository

--- src/main/java/com/projeto/casadocodigo/repository/AutorRepository.java
+++ src/main/java/com/projeto/casadocodigo/repository/AutorRepository.java
@@ -1,2 +1,12 @@
-package com.projeto.casadocodigo.repository;public class AutorRepository {
+package com.projeto.casadocodigo.repository;
+
+import com.projeto.casadocodigo.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+
+
 }
