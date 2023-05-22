fea829abaae59410fa82e4d91ad4327dcf128070
viniciuspch <vinicius.geral2018@gmail.com>
2023-05-22 15:00:09 -0300

    Implementar entidade e seus atributos

--- src/main/java/com/projeto/casadocodigo/model/Autor.java
+++ src/main/java/com/projeto/casadocodigo/model/Autor.java
@@ -1,2 +1,63 @@
-package com.projeto.casadocodigo.model;public class Autor {
+package com.projeto.casadocodigo.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(max = 400)
+    private String descricao;
+
+    @NotNull
+
+    private LocalDateTime instanteAutor;
+
+    public void setInstanteAutor(LocalDateTime instanteAutor) {
+        this.instanteAutor = instanteAutor;
+    }
+    public Autor() {
+        this.instanteAutor = LocalDateTime.now();
+    }
+    public LocalDateTime getInstanteAutor() {
+        return instanteAutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
 }
